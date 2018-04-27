5d08143c812d2dc7e34eb5a3ffda6405823c5568
PhamHuuTai <phamhuutai95@gmail.com>
2018-04-27 11:09:01 +0700

    Mình tạo class này để đặt mấy cái hàm mà xử lý data mà hay dùng chung. Trong hàm này mình mới viết 1 phương thức đó là ConvertLst dùng để chuyển HttpResponseMessage sang list object với kiểu dữ liệu truyền vào (giải quyết được vấn đề chữ "C" tự add vào feild khi tạo data entity)

---
+++ Project_16HCB_View/Project_16HCB_View/Models/Common.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_16HCB_View.Models
+{
+    /// <summary>
+    /// This class set functions general
+    /// </summary>
+    public class Common
+    {
+        /// <summary>
+        /// convert response result api to list obj type T
+        /// </summary>
+        /// <typeparam name="T">object type output</typeparam>
+        /// <param name="res">HttpResponseMessage</param>
+        /// <param name="lstReceiver">list output</param>
+        public static void ConvertLst<T>(System.Net.Http.HttpResponseMessage res, ref List<T> lstReceiver)
+        {
+            string strResult = res.Content.ReadAsStringAsync().Result;
+            strResult = strResult.Replace("C", "");
+
+            if (strResult.IndexOf('[') == -1)
+            {
+                strResult = strResult.Insert(1, "[");
+                strResult = strResult.Insert(strResult.Length - 1, "]");
+            }
+            
+            var json_serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+            lstReceiver = json_serializer.Deserialize<List<T>>(json_serializer.Deserialize<string>(strResult));
+        }
+    }
+}
