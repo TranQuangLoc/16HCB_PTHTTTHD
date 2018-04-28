902177315a5765023c8fc019e6c832687e19a2ee
Van Bui <tichondrius.btv@gmail.com>
2018-04-28 04:14:57 -0700

    Hoan thanh xong chuc nang report sl sv nghi

---
+++ Project_16HCB/Project_16HCB/Controllers/ReportController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Project_16HCB.Models;
+
+namespace Project_16HCB.Controllers
+{
+
+    public class ReportStudentResult {
+        public int MSSV { get; set; }
+        public string Username { get; set; }
+        public string TenLop { get; set; }
+        public int NamHoc { get; set; }
+        public string MonHoc { get; set; }
+        public string HocKy { get; set; }
+        public int SLNghi { get; set; }
+    }
+    public class ReportController : ApiController
+    {
+        // GET: api/Default
+        public List<ReportStudentResult> Get(int Nam = -1, int HocKy = -1, int MonHoc = -1, int LopHoc = -1)
+        {
+            using (var db = new Project_16HCB_CSDLEntities())
+            {
+                List<ReportStudentResult> results = new List<ReportStudentResult>();
+
+                results = db.Database.SqlQuery<ReportStudentResult>("sp_ReportStudent @Nam, @HocKy, @MonHoc, @LopHoc",
+                    new SqlParameter("Nam", Nam),
+                    new SqlParameter("HocKy", HocKy),
+                    new SqlParameter("MonHoc", MonHoc),
+                    new SqlParameter("LopHoc", LopHoc)).ToList();
+                return results;
+            }
+        }
+    }
+}
